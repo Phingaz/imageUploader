4eb6463065dc4781ddb0f50bb60221fdb1b6f8c8
Mfm Sao Paulo <piinoya@gmail.com>
2023-07-23 12:07:19 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+
+  const [image, setImage] = useState({})
+  const [state, setState] = useState({
+    isUploaded: false,
+    imageFile: {}
+  })
+
+  const handleChange = (e) => {
+    setState({
+      isUploaded: false
+    })
+    setImage(URL.createObjectURL(e.target.files[0]));
+    setState({
+      isUploaded: true,
+      imageFile: e.target.files[0]
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(image)
+    setState({
+      isUploaded: true
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='content'>
+        <h2>Upload your image</h2>
+        <p>Acceptable file format (.jpeg, .png, etc.)</p>
+
+        <form onSubmit={handleSubmit} className='form'>
+          <input
+          id='image'
+          name='image'
+          type='file'
+          accept='image/*'
+          onChange={handleChange}
+          />
+          <label htmlFor='image'>
+            <img
+              src={image}
+              alt={image.name}
+            />
+            {!state.isUploaded
+             ?
+              'Click here to choose your file'
+              :
+              state.imageFile.name
+              }
+            </label>
+
+          <button>Submit</button>
+        </form>
+
+      </div>
     </div>
   );
 }
